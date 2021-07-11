7fe7163cee40ef42907290b4368eb34e09cd5c03
hatsumi3 <al.usamiku39@gmail.com>
2021-07-11 15:58:26 +0900

    Next.js Tutorial.
    BASICS
    - Create a Next.js App
    - Navigate Between Pages
    - Assets, Metadata, and CSS

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/global.css';
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
+
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
